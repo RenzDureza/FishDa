067f5ea1a6232e543324c4c4c3de5531bf0ee338
renzDureza <durezarenz@gmail.com>
2026-01-30 10:33:08 +0800

    organized as a layered architecture

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import db from "./config/database.js";
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config({ path: ".env" });
 
@@ -11,21 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.post("/users", async (req, res) => {
-  const { username, email, password } = req.body;
-  const sql =
-    "INSERT INTO `users` (`username`, `email`, `password`) VALUES (?, ?, ?)";
-
-  try {
-    const [result] = await db.query(sql, [username, email, password]);
-    res.status(201).json({
-      message: "User Created",
-      id: result.insertId,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`App is listening on Port: ${PORT}`);
