b62801dfc19887d27f1836173338b6ca84c81741
ItsWaine14 <jwainebravo@gmail.com>
2026-02-02 20:25:08 +0800

    Implemented Nativewind

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>wussup nibblers</Text>
+    <View className="flex-1 items-center justify-center bg-red-400">
+      <Text className="text-white">wussup nibblers</Text>
     </View>
   );
 }
