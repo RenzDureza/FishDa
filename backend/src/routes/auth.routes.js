14ec1da0b81fb604d172d82ff37cb9d12772d057
ItsWaine14 <jwainebravo@gmail.com>
2026-04-14 15:21:58 +0800

    Add Email Verification

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { register, login } from "../controllers/auth.controllers.js";
 import { showUsers } from "../controllers/admin.controllers.js";
 import { auth, requireRole } from "../utils/auth.js";
+import { verify } from "jsonwebtoken";
 
 const router = Router();
 
 router.post("/register", register);
 router.get("/admin", auth, requireRole("admin"), showUsers);
 router.post("/login", login);
+router.get("/verify-email", verifyEmai);
 
 export default router;
