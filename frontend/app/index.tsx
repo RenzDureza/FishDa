cb49c3ea1a5e81ee8bf93cf64aed8c7537add7c0
Kaa1iru <kyle120203@gmail.com>
2026-04-24 20:04:46 +0800

    Fixes for token/session expiry, logged in redirects to home

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -2,11 +2,19 @@ import { Redirect } from "expo-router";
 import { useAuth } from "../utils/authContext";
 
 export default function Index() {
-	const { isLoggedIn } = useAuth();
+	const { isReady, isLoggedIn, role } = useAuth();
 
-	if (isLoggedIn) {
-		return <Redirect href="/(user)/(drawer)/home" />;
-	} else {
+	if (!isReady){
+		return null;
+	}
+
+	if (!isLoggedIn) {
 		return <Redirect href="/(auth)/signin" />;
 	}
+
+	if (role === "admin"){
+		return <Redirect href="/(admin)/(stack)/adminHome" />;
+	}
+
+	return <Redirect href="/(user)/(drawer)/home" />;
 }
